1b467e6627ad05d9a9008eacd73931740d8d5172
Justin Hempsey <flinestone@rocketmail.com>
2016-02-17 10:50:51 -0800

    working on master slave

--- part2/mandelbrot_ms.cc
+++ part2/mandelbrot_ms.cc
@@ -12,12 +12,12 @@
 
 using namespace std;
 
-#define WIDTH = 1000
-#define HEIGHT = 1000
+#define WIDTH 1000
+#define HEIGHT 1000
 
 #define REQUEST 1
 #define DATA 0
-
+//TRY CALLOC
  float
  **contig2dArray(int rows, int cols) {
    float *data = (float *)malloc(rows*cols*sizeof(float));
@@ -83,38 +83,46 @@ using namespace std;
      //Master
      MPI_Status status;
      int count;
-     int chunk_details[2];
-
+    int *chunk_details;
+    chunk_details = (int*)malloc(sizeof(int)*2);
      //alloc image array
      gil::rgb8_image_t img(height, width);
      auto img_view = gil::view(img);
 
      //data array for the temp chunk
      float **tempdata;
+     int *temp; //junk variable
+
      tempdata = contig2dArray(width, height);
-     // while (chunk_details[1] < height)
-     // {
-       int *temp; //junk variable
+     //set chunk size
+     chunk_details[0] = 500;
+     //set chunk row start
+     chunk_details[1] = 0;
+
+     while (chunk_details[1] + chunk_details[0] <= height)
+     {
        printf("Waiting for request\n");
        MPI_Recv(&temp, 1,
          MPI_INT, MPI_ANY_SOURCE, REQUEST, MPI_COMM_WORLD, &status);
-       //set chunk size
-       chunk_details[0] = 500;
-       //set chunk row start
-       chunk_details[1] = 0;
-       //send the chunk details out
+              //send the chunk details out
        printf("Sending next chunk...\n");
-       MPI_Send(&chunk_details, 2, MPI_INT, status.MPI_SOURCE, DATA, MPI_COMM_WORLD);
+       MPI_Send(&(chunk_details[0]), 2, MPI_INT, status.MPI_SOURCE, DATA, MPI_COMM_WORLD);
        printf("Waiting for chunk...\n");
-       MPI_Recv(&(tempdata[0][chunk_details[0]]), width * chunk_details[0], MPI_FLOAT,
+       MPI_Recv(&(tempdata[0][0]), width * chunk_details[0], MPI_FLOAT,
                 status.MPI_SOURCE, DATA, MPI_COMM_WORLD, &status);
-     //   //increment start row
-     //   chunk_details[1] += chunk_details[0];
+       //increment start row
+       break;
+       chunk_details[1] += chunk_details[0];
      //   return 1;
-     // }
-     // chunk_details[0] = -1;
-     // chunk_details[1] = -1;
-     // MPI_Send(&chunk_details, 2, MPI_INT, 1, REQUEST, MPI_COMM_WORLD);
+     }
+     printf("Listening for final request...\n");
+     MPI_Recv(&temp, 1,
+       MPI_INT, MPI_ANY_SOURCE, REQUEST, MPI_COMM_WORLD, &status);
+     chunk_details[0] = -1;
+     chunk_details[1] = -1;
+     printf("Sending end.\n");
+     MPI_Send(&(chunk_details[0]), 2, MPI_INT, 1, DATA, MPI_COMM_WORLD);
+     printf("Sent. Rendering...\n");
 
      //render the image
      for (int i = 0; i < height; ++i)
@@ -126,6 +134,7 @@ using namespace std;
      }
      free(tempdata[0]);
      free(tempdata);
+     free(chunk_details);
 
      gil::png_write_view("mandelbrot-test-ms.png", const_view(img));
 
@@ -134,31 +143,33 @@ using namespace std;
    {
      float **data;
      int *junk;
-     int chunk_details[2];
+    int *chunk_details;
+    chunk_details = (int*)malloc(sizeof(int)*2);
      MPI_Status status;
      int done = 0;
 
-
-   //   while (done == 0)
-   //   {
-
-   //    printf("Done sleeping.\n");
+     while (done == 0)
+     {
+       printf("Sending Request.\n");
        MPI_Send(&junk, 1, MPI_INT, 0, REQUEST, MPI_COMM_WORLD);
-       MPI_Recv(&chunk_details, 2, MPI_INT, 0, DATA, MPI_COMM_WORLD, &status);
+       printf("Waiting for chunk_details.\n");
+       MPI_Recv(&(chunk_details[0]), 2, MPI_INT, 0, DATA, MPI_COMM_WORLD, &status);
        printf("details[0]: %d  details[1]: %d\n",chunk_details[0], chunk_details[1]);
-   //     if (chunk_details[0] == -1 && chunk_details[1] == -1)
-   //     {
-   //       done = 1;
-   //     }
-   //     else
-   //     {
+       //Determine if were done or not
+       if (chunk_details[0] == -1 && chunk_details[1] == -1)
+       {
+         done = 1;
+       }
+       else
+       {
          data = contig2dArray(width, chunk_details[0]);
 
          printf("Calculating row:%d size: %d\n", chunk_details[1], chunk_details[0]);
-         y = minY + (it * double(chunk_details[1]));
+         y = minY;//+ (it * double(chunk_details[1]));
          for(int i = 0; i < chunk_details[0]; ++i)
          {
            x = minX;
+           printf("i: %d\n", i);
            for (int j = 0; j < width; ++j)
            {
              data[j][i] = mandelbrot(x, y) / 512.0;
@@ -169,12 +180,12 @@ using namespace std;
          printf("Sending data...\n");
          //Send data
          MPI_Send(&(data[0][0]), (width * chunk_details[0]), MPI_FLOAT, 0, DATA, MPI_COMM_WORLD);
-   //     }
-   //     return 2;
-   //   }
+         free(data[0]);
+         free(data);
+       }
+     }
      //free
-     free(data[0]);
-     free(data);
+     free(chunk_details);
    }
    MPI_Finalize();
    return 0;
